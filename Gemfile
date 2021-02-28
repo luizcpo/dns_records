8241eb597f261c95f6943c3a8b87f0c82c6b4591
Luiz Oliveira <luiz.paula@catho.com>
2021-02-28 15:45:44 -0300

    Including functionality to the endpoints and excluding unnecessary files

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ ruby '2.6.5'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.4', '>= 5.2.4.5'
+# Rack::Cors provides support for Cross-Origin Resource Sharing (CORS) for Rack compatible web applications.
+gem 'rack-cors'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use Puma as the app server
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+DnsRecord.create(ip_address: '1.1.1.1', hostnames_attributes: [
+    { hostname: 'lorem.com' },
+    { hostname: 'ipsum.com' },
+    { hostname: 'dolor.com' },
+    { hostname: 'amet.com' }
+])
+DnsRecord.create(ip_address: '2.2.2.2', hostnames_attributes: [
+    { hostname: 'ipsum.com' }
+])
+DnsRecord.create(ip_address: '3.3.3.3', hostnames_attributes: [
+    { hostname: 'ipsum.com' },
+    { hostname: 'dolor.com' },
+    { hostname: 'amet.com' }
+])
+DnsRecord.create(ip_address: '4.4.4.4', hostnames_attributes: [
+    { hostname:'ipsum.com' },
+    { hostname:'dolor.com' },
+    { hostname:'sit.com' },
+    { hostname:'amet.com' }
+])
+DnsRecord.create(ip_address: '5.5.5.5', hostnames_attributes: [
+    { hostname: 'dolor.com' },
+    { hostname: 'sit.com' }
+])
